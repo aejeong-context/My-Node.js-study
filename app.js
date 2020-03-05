fdbf6183744f4dbe667b9d02f5d584ad71fd0e52
aejeong <aejeong-context@kakao.com>
2020-03-05 20:45:42 +0900

    node_1day

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-const tutorial = require('./tutorial')
-console.log(tutorial);
-console.log(tutorial(1,1));
+ const tutorial = require('./tutorial');
+console.log(tutorial.sum(1,1));
+console.log(tutorial.PI);
+console.log(new tutorial.MathObj());
+
+
+const Eventmitter = require('event');
+const eventmitter  = new Eventmitter();
+eventmitter.on('tutorial',(num1,num2)=>{
+    console.log(num1+num2);
+});
+
+eventmitter.emit('tutorial',1,2);
--- tutorial.js
+++ tutorial.js
@@ -1,2 +1,15 @@
 const sum = (num1,num2)=>num1+num2;
-module.exports=sum;
+const PI=3.14;
+class MathObj{
+   constructor(){
+        console.log('obj created');
+    }
+}
+
+//module이란 외부 영향을 받지 않고 독립된, 재사용 가능한 코드 묶음
+module.exports.sum=sum;
+module.exports.PI=PI;
+module.exports.MathObj=MathObj;
+//못생겼으니 하나로 묶어보자
+module.exports= {sum: sum , PI:PI, MathObj : MathObj};
+
