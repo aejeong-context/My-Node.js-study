48de73a6e72143e7a5c000b36420b749bed6285a
aejeong <aejeong-context@kakao.com>
2020-03-06 18:09:23 +0900

    node_2day File System Module 1

---
+++ app2.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+
+//파일 생성
+
+//파일이름, 내용
+// fs.writeFile('example.text',"this is an example, 이것은 예제이다!",(err)=>{
+//     if(err)
+//     console.log(err);
+//     else{
+//     console.log('File successfully created');
+//     //파일 내용을 출력하기 위함 
+//     fs.readFile('example.text','utf-8',(err,file)=>{
+//         if(err)
+//             console.log(err);
+//             else
+//             console.log(file);
+//     });
+//     }
+// });
+
+//파일명 변경
+
+// fs.rename('example.text','example2.text',(err)=>{
+//    if(err)
+//         console.log(err);
+//     else
+//         console.log('successfully renamed the file'); 
+// });
+
+// fs.appendFile('example2.text','Some data being appended!',(err)=>{
+//     if(err)
+//         console.log(err);
+//     else
+//         console.log('Successfully appended data to file');
+// });
+
+//파일 삭제
+
+// fs.unlink('example2.text',(err)=>{
+//     if(err)
+//         console.log(err);
+//     else
+//         console.log('Successfully deleted the file');
+// });
+
+
+
+
